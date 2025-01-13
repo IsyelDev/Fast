ffc0e4634737be47097ff963d3b116932446cee2
Totti Code <163216274+IsyelDev@users.noreply.github.com>
2025-01-13 07:07:53 -0500

    Update of register in user root.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,16 @@ router.get("/about", (req, res) => {
   res.render("about", { title: "About First Node Website" });
 });
 
+
+router.post("/register", (req, res) => {
+  const username = req.body.username; // Extraemos 'username' del cuerpo de la solicitud
+
+  username
+    ? res.status(200).send(`Welcome, ${username}!`)
+    : res.status(400).send("Error: Username is required");
+});
+
+
 router.get("/contact", (req, res) => {
   res.render("contact", { title: "Contact Page" });
 });
